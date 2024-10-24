6c65ee45d8fcd5ea11680fd50b7b9de7d32dc786
Benoy Maniara <benoy.maniara@walmart.com>
2024-10-24 19:16:32 -0400

    cp init file to static/js folder

---
+++ build/static/js/init.js
@@ -0,0 +1,32 @@
+(async () => {
+  const loadScript = async (src) => {
+    return new Promise((res) => {
+      const script = document.createElement('script');
+      script.src = src;
+      script.onload = res;
+      document.body.appendChild(script);
+    });
+  };
+  
+  const loadStylesheet = async (src) => {
+    return new Promise((res) => {
+      const link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = src;
+      link.onload = res;
+      document.head.appendChild(link);
+    });
+  };
+
+
+  const loadResources = async () => {
+    return Promise.all([
+      "https://cdn.jsdelivr.net/gh/maniarab/bookmarklet-jira@main/build/static/js/main.71e4cfc2.js",
+    ].map(loadScript).concat([
+      "https://cdn.jsdelivr.net/gh/maniarab/bookmarklet-jira@main/build/static/css/main.f855e6bc.css"
+    ].map(loadStylesheet)))
+  };
+
+  console.log("Loading javascript and css...");
+  await loadResources();
+})();
--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -15,7 +15,8 @@
   };
   
 
-  const initUrl = "https://cdn.jsdelivr.net/gh/maniarab/bookmarklet-jira@main/src/init.js";
+  // const initUrl = "https://cdn.jsdelivr.net/gh/maniarab/bookmarklet-jira@main/src/init.js";
+  const initUrl = "https://cdn.jsdelivr.net/gh/maniarab/bookmarklet-jira@main/build/static/js/init.js";
 
   const loadResources = async () => {
     return Promise.all([
@@ -30,5 +31,5 @@
 
 
 /*
-javascript:(function()%7B(async%20()%20%3D%3E%20%7B%0A%20%20const%20createReactRoot%20%3D%20async%20()%20%3D%3E%20%7B%0A%20%20%20%20const%20reactDiv%20%3D%20document.createElement('div')%3B%0A%20%20%20%20reactDiv.setAttribute(%22id%22%2C%20%22bookmarklet-jira%22)%3B%0A%20%20%20%20document.body.appendChild(reactDiv)%3B%0A%20%20%7D%3B%0A%0A%20%20const%20loadScript%20%3D%20async%20(src)%20%3D%3E%20%7B%0A%20%20%20%20return%20new%20Promise((res)%20%3D%3E%20%7B%0A%20%20%20%20%20%20const%20script%20%3D%20document.createElement('script')%3B%0A%20%20%20%20%20%20script.src%20%3D%20src%3B%0A%20%20%20%20%20%20script.onload%20%3D%20res%3B%0A%20%20%20%20%20%20document.body.appendChild(script)%3B%0A%20%20%20%20%7D)%3B%0A%20%20%7D%3B%0A%20%20%0A%0A%20%20const%20initUrl%20%3D%20%22https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2Fmaniarab%2Fbookmarklet-jira%40main%2Fsrc%2Finit.js%22%3B%0A%0A%20%20const%20loadResources%20%3D%20async%20()%20%3D%3E%20%7B%0A%20%20%20%20return%20Promise.all(%5B%0A%20%20%20%20%20%20initUrl%0A%20%20%20%20%5D.map(loadScript))%3B%0A%20%20%7D%3B%0A%0A%20%20console.log(%22Loading%20bookmarklet...%22)%3B%0A%20%20await%20createReactRoot()%3B%0A%20%20await%20loadResources()%3B%0A%7D)()%3B%7D)()%3B
+javascript:(function()%7B(async%20()%20%3D%3E%20%7B%0A%20%20const%20createReactRoot%20%3D%20async%20()%20%3D%3E%20%7B%0A%20%20%20%20const%20reactDiv%20%3D%20document.createElement('div')%3B%0A%20%20%20%20reactDiv.setAttribute(%22id%22%2C%20%22bookmarklet-jira%22)%3B%0A%20%20%20%20document.body.appendChild(reactDiv)%3B%0A%20%20%7D%3B%0A%0A%20%20const%20loadScript%20%3D%20async%20(src)%20%3D%3E%20%7B%0A%20%20%20%20return%20new%20Promise((res)%20%3D%3E%20%7B%0A%20%20%20%20%20%20const%20script%20%3D%20document.createElement('script')%3B%0A%20%20%20%20%20%20script.src%20%3D%20src%3B%0A%20%20%20%20%20%20script.onload%20%3D%20res%3B%0A%20%20%20%20%20%20document.body.appendChild(script)%3B%0A%20%20%20%20%7D)%3B%0A%20%20%7D%3B%0A%20%20%0A%0A%20%20%2F%2F%20const%20initUrl%20%3D%20%22https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2Fmaniarab%2Fbookmarklet-jira%40main%2Fsrc%2Finit.js%22%3B%0A%20%20const%20initUrl%20%3D%20%22https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2Fmaniarab%2Fbookmarklet-jira%40main%2Fbuild%2Fstatic%2Fjs%2Finit.js%22%3B%0A%0A%0A%20%20const%20loadResources%20%3D%20async%20()%20%3D%3E%20%7B%0A%20%20%20%20return%20Promise.all(%5B%0A%20%20%20%20%20%20initUrl%0A%20%20%20%20%5D.map(loadScript))%3B%0A%20%20%7D%3B%0A%0A%20%20console.log(%22Loading%20bookmarklet...%22)%3B%0A%20%20await%20createReactRoot()%3B%0A%20%20await%20loadResources()%3B%0A%7D)()%3B%7D)()%3B
 */
